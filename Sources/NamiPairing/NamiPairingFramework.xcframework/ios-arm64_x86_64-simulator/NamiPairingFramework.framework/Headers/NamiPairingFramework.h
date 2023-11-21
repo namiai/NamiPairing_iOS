ee9e05f553d50533a74e88dac230aaf8da638185
runner <runner@Mac-1700553576338.local>
2023-11-21 08:16:58 +0000

    release 1.4

---
+++ Sources/NamiPairing/NamiPairingFramework.xcframework/ios-arm64_x86_64-simulator/NamiPairingFramework.framework/Headers/NamiPairingFramework.h
@@ -0,0 +1,18 @@
+//
+//  NamiPairingFramework.h
+//  NamiPairingFramework
+//
+//  Created by Yachin Ilya on 17.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NamiPairingFramework.
+FOUNDATION_EXPORT double NamiPairingFrameworkVersionNumber;
+
+//! Project version string for NamiPairingFramework.
+FOUNDATION_EXPORT const unsigned char NamiPairingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NamiPairingFramework/PublicHeader.h>
+
+
---
+++ Sources/NamiPairing/StandardPairingUI.xcframework/ios-arm64/StandardPairingUI.framework/Headers/StandardPairingUI.h
@@ -0,0 +1,18 @@
+//
+//  StandardPairingUI.h
+//  StandardPairingUI
+//
+//  Created by Yachin Ilya on 12.06.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StandardPairingUI.
+FOUNDATION_EXPORT double StandardPairingUIVersionNumber;
+
+//! Project version string for StandardPairingUI.
+FOUNDATION_EXPORT const unsigned char StandardPairingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StandardPairingUI/PublicHeader.h>
+
+
