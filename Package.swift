0c27041b53110073c24613b94197a6849b5f318c
Ilya <uin153974748@gmail.com>
2023-11-21 17:55:54 +0800

    removed dependencies

--- Package.swift
+++ Package.swift
@@ -14,10 +14,6 @@ let package = Package(
             name: "NamiPairing",
             targets: ["NamiPairingFramework", "StandardPairingUI"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", exact: "1.20.3"),
-        .package(url: "https://github.com/weitieda/bottom-sheet", exact: "1.0.8"),
-    ],
     targets: [
         .binaryTarget(name: "NamiPairingFramework", path: "Sources/NamiPairing/NamiPairingFramework.xcframework"),
         .binaryTarget(name: "StandardPairingUI", path: "Sources/NamiPairing/StandardPairingUI.xcframework"),
