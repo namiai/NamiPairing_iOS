22e3bc1f8b1c84d59776411c87a71becb31095e5
Tran Viet Hoang <hoang@nami.ai>
2024-05-02 17:45:12 +0700

    adding specific swift versions

---
+++ Package@swift-5.8.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NamiPairing",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_15),
+    ],
+    products: [
+        .library(
+            name: "NamiPairing",
+            targets: ["NamiPairingFramework", "StandardPairingUI", "DeviceConnections"]),
+        .library(
+            name: "NamiPairing-NoUI", 
+            targets: ["NamiPairingFramework", "DeviceConnections"])
+    ],
+    targets: [
+        .binaryTarget(name: "NamiPairingFramework", path: "Sources/NamiPairing/NamiPairingFramework.xcframework"),
+        .binaryTarget(name: "StandardPairingUI", path: "Sources/NamiPairing/StandardPairingUI.xcframework"),
+        .binaryTarget(name: "DeviceConnections", path: "Sources/NamiPairing/DeviceConnections.xcframework")
+    ]
+)
