99c1ccd219614b1010689de82e4dfc5589e3a470
Tran Viet Hoang <hoang@nami.ai>
2025-07-21 19:06:08 +0700

    adding swift-6.1

---
+++ Package@swift-6.1.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NamiPairing",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_15),
+    ],
+    products: [
+        .library(
+            name: "NamiPairing",
+            targets: ["NamiPairingFramework", "StandardPairingUI"]),
+        .library(
+            name: "NamiPairing-NoUI", 
+            targets: ["NamiPairingFramework"])
+    ],
+    targets: [
+        .binaryTarget(name: "NamiPairingFramework", path: "Sources/NamiPairing/NamiPairingFramework.xcframework"),
+        .binaryTarget(name: "StandardPairingUI", path: "Sources/NamiPairing/StandardPairingUI.xcframework"),
+    ]
+)
