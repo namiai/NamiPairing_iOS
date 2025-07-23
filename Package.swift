9154f0a0ccfb9f0b3029d2f23aabd1083b64ba65
Tran Viet Hoang <hoang@nami.ai>
2025-07-23 11:54:05 +0700

    creating wrapper target to add divkit as transitive dependency

--- Package.swift
+++ Package.swift
@@ -12,13 +12,33 @@ let package = Package(
     products: [
         .library(
             name: "NamiPairing",
-            targets: ["NamiPairingFramework", "StandardPairingUI"]),
+            targets: ["NamiPairingCore", "StandardPairingUI"]
+        ),
         .library(
             name: "NamiPairing-NoUI", 
-            targets: ["NamiPairingFramework"])
+            targets: ["NamiPairingCore"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Yandex/DivKit.git", from: "31.0.0")
     ],
     targets: [
-        .binaryTarget(name: "NamiPairingFramework", path: "Sources/NamiPairing/NamiPairingFramework.xcframework"),
-        .binaryTarget(name: "StandardPairingUI", path: "Sources/NamiPairing/StandardPairingUI.xcframework"),
+        .binaryTarget(
+            name: "NamiPairingFramework", 
+            path: "Sources/NamiPairing/NamiPairingFramework.xcframework"
+        ),
+        .binaryTarget(
+            name: "StandardPairingUI", 
+            path: "Sources/NamiPairing/StandardPairingUI.xcframework"
+        ),
+        .target(
+            name: "NamiPairingCore",
+            dependencies: [
+                "NamiPairingFramework",
+                .product(name: "DivKit", package: "DivKit")
+            ],
+            path: "Sources/NamiPairing/NamiPairingCore",
+            publicHeadersPath: "."
+        ),
     ]
 )
