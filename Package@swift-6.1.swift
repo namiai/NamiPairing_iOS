ccb78037aed7293ba20fbd443352b1ae53e9bdb5
Tran Viet Hoang <hoang@nami.ai>
2025-12-08 11:21:58 +0700

    update package files

--- Package@swift-6.1.swift
+++ Package@swift-6.1.swift
@@ -7,29 +7,15 @@ let package = Package(
     name: "NamiPairing",
     platforms: [
         .iOS(.v14),
-        .macOS(.v10_15),
     ],
     products: [
         .library(
             name: "NamiPairing",
-            targets: ["NamiPairingCore", "StandardPairingUI"]),
-        .library(
-            name: "NamiPairing-NoUI", 
-            targets: ["NamiPairingCore"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-spm.git", .upToNextMajor(from: "4.4.3")),
+            targets: ["NamiPairingFramework"]),
     ],
     targets: [
-        .binaryTarget(name: "NamiPairingFramework", path: "Sources/NamiPairing/NamiPairingFramework.xcframework"),
-        .binaryTarget(name: "StandardPairingUI", path: "Sources/NamiPairing/StandardPairingUI.xcframework"),
-        .target(
-          name: "NamiPairingCore",
-          dependencies: [
-            "NamiPairingFramework",
-            .product(name: "Lottie", package: "lottie-spm"),
-          ],
-          path: "Sources/NamiPairing/NamiPairingCore"
-        ),
+        .binaryTarget(
+            name: "NamiPairingFramework",
+            path: "Sources/NamiPairing/NamiPairingFramework.xcframework"),
     ]
 )
